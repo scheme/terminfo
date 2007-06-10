213b172bb6a6f8eeab4dabeb8b9f24b0a4449726
Duncan Mak <duncan@a-chinaman.com>
2007-06-10 05:08:00 -0400

    Fixed module definition.

--- package.scm
+++ package.scm
@@ -1,25 +1,21 @@
-(define-interface terminfo-interface
-  (compound-interface terminfo-core-interface terminfo-capabilities-interface))
-
 (define-interface terminfo-core-interface 
   (export *terminfo* *terminfo-directories* set-terminal))
 
 (define-interface terminfo-capabilities-interface
   (export terminfo-capability))
 
-(define-structure terminfo terminfo-interface
-  (open terminfo-core terminfo-capabilities))
+(define-interface terminfo-interface
+  (compound-interface terminfo-core-interface terminfo-capabilities-interface))
 
 (define-structure terminfo-core terminfo-core-interface
   (open scheme-with-scsh
-        srfi-6 srfi-9 srfi-16
-        tables)
+        srfi-6 srfi-9 srfi-13 srfi-16)
   (files terminfo))
 
 (define-structure terminfo-capabilities terminfo-capabilities-interface
+  (open scheme-with-scsh tables terminfo-core)
   (files terminfo-capabilities))
 
-
-
-
+(define-structure terminfo terminfo-interface
+  (open terminfo-core terminfo-capabilities))
 
