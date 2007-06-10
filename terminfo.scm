7742d90bc7c6d9b02e68f4c98dea2e085e20cf09
Duncan Mak <duncan@a-chinaman.com>
2007-06-10 16:54:49 -0400

    Move the call to terminfo-filename from load-terminfo to set-terminal.

--- terminfo.scm
+++ terminfo.scm
@@ -12,7 +12,7 @@
 (define-record-type terminfo
   (make-terminfo names booleans numbers strings)
   terminfo?
-  (names terminfo-names)
+  (names    terminfo-names)
   (booleans terminfo-booleans set-terminfo-booleans!)
   (numbers  terminfo-numbers  set-terminfo-numbers!)
   (strings  terminfo-strings  set-terminfo-strings!))
@@ -25,7 +25,7 @@
                (file    (path-list->file-name (list basedir initial name))))
           (cond ((file-not-exists? file) 
                  (error "Cannot find terminfo named " name))
-                ; TODO: file-exists? returns only the relative path
+                ; TODO: returns only the relative path
                 ((file-symlink? file) (read-symlink file))
                 ((file-readable? file)  file)
                 (else (loop (cdr dirs))))))))
@@ -47,7 +47,7 @@
   (let loop ((c      (read-char stream))
              (s      "")
              (result '()))
-    (cond ((or (eof-object? c) (zero? (char->integer c)))
+    (cond (((zero? (char->ascii c)))
            (reverse (cons s result)))
           ((char=? c #\|)
            (loop (read-char stream)
@@ -58,7 +58,7 @@
                       result)))))
 
 (define (load-terminfo name)
-  (call-with-input-file (terminfo-filename name)
+  (call-with-input-file name
     (lambda (stream)
       (let* ((magic         (read-short stream))
              (sznames       (read-short stream))
@@ -72,7 +72,7 @@
              (strings       (make-vector szstrings -1))
              (stringtable   (make-string szstringtable)))
         (if (not (= magic #o432))
-            (error "file format is invalid."))
+            (error "this file is invalid: " name))
         (do ((i 0 (+ i 1))) ((>= i szbooleans))
           (vector-set! booleans i (not (zero? (read-byte stream)))))
         (if (odd? (+ sznames szbooleans))
@@ -94,7 +94,8 @@
         (make-terminfo names booleans numbers strings)))))
 
 (define (set-terminal . args)
-  (let ((name (if (not (null? args))
-                  (car args)
-                  (getenv "TERM"))))
-    (set! *terminfo* (load-terminfo name))))
+  (let* ((term     (if (not (null? args))
+                       (car args)
+                       (getenv "TERM")))
+         (filename (terminfo-filename term)))
+    (set! *terminfo* (load-terminfo filename))))
