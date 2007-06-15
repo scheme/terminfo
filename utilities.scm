d329a0c048390e62c1f45d1477fbe0c9c0068929
Duncan Mak <duncan@a-chinaman.com>
2007-06-14 23:42:59 -0400

    Put utilities functions / macros here.

---
+++ utilities.scm
@@ -0,0 +1,50 @@
+;;;
+;;; utilities.scm
+;;;
+
+(define-syntax process-state
+  (syntax-rules (accept ->)
+    ((_ accept)
+     (lambda (stream) (null? stream)))
+    ((_ (label -> target) ...)
+     (lambda (stream)
+       (if
+        (null? stream) #f
+        (case (car stream)
+          ((label) (target (cdr stream)))
+          ...
+          (else #f)))))))
+
+(define-syntax automaton
+  (syntax-rules (:)
+    ((_ init-state
+        (state : response ...)
+        ...)
+     (letrec ((state
+               (process-state response ...))
+              ...)
+       init-state))))
+
+(define-syntax push
+  (syntax-rules ()
+    ((_ item list)
+     (set! list (cons item list)))))
+
+(define-syntax pop
+  (syntax-rules ()
+    ((_ list)
+     (let ((value (car list)))
+       (set! list (cdr list))
+       value))))
+
+(define-syntax when
+  (syntax-rules ()
+    ((_ condition action ...)
+     (if condition
+         (begin action ...)))))
+
+(define (char->digit c)
+  (if (char-numeric? c)
+      (- (char->integer c) (char->integer #\0))
+      -1))
+
