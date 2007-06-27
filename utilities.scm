2f1d4665a721fc9bb404fbba2132d1e6260bd764
Duncan Mak <duncan@a-chinaman.com>
2007-06-27 03:06:17 -0400

    Add 1+ from MIT Scheme for easy increments.

--- utilities.scm
+++ utilities.scm
@@ -6,3 +6,8 @@
   (if (char-digit? c)
       (- (char->integer c) (char->integer #\0))
       -1))
+
+(define (1+ digit)
+  (if (number? digit)
+      (+ 1 digit)
+      (error digit " is not a number")))
