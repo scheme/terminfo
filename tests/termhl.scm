eaa563c09eea7834d9c48b359a25ffc2c36b57ae
Duncan Mak <duncan@a-chinaman.com>
2007-07-09 17:05:10 -0400

    Add sample program

---
+++ tests/termhl.scm
@@ -0,0 +1,32 @@
+;;; Tests the terminfo library
+;;;
+;;; Based on: http://uw713doc.sco.com/en/SDK_charm/_An_Example_terminfo_Program.html
+;;;
+;;; ,open terminfo
+;;;
+
+(define text "This is UunderlinedU.\nThis is BboldB.\n")
+(define underlined? #f)
+(define bold? #f)
+
+(define (toggle flag)
+  (case flag
+    ((underline)
+     (set! underlined? (not underlined?))
+     (tputs (if underlined?
+                (enter-underline-mode)
+                (exit-underline-mode))))
+    ((bold)
+     (set! bold? (not bold?))
+     (tputs (if bold?
+                (enter-bold-mode)
+                (exit-attribute-mode))))))
+
+(setup-terminal)
+(string-for-each
+ (lambda (c)
+   (cond
+    ((char=? #\U c) (toggle 'underline))
+    ((char=? #\B c) (toggle 'bold))
+    (else (display c))))
+ text)
